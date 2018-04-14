0f1e726ae055c51c9cc84e80f550f84c8c0b17b0
ldlchina <ldlchina@gmail.com>
2018-04-14 20:54:55 +0800

    Retrive stock code from sse.

--- get_all_stocks/get_all_stocks.py
+++ get_all_stocks/get_all_stocks.py
@@ -1,12 +1,14 @@
-# Retrive stock ids and data from 'http://quote.eastmoney.com'.
+# Retrive stock codes and data from 'http://quote.eastmoney.com'.
 import os
 import sys
 import urllib.request
 import re
 
-# Retrive all stock ids
-def urlTolist(url):
-    allCodeList = []
+# Retrive all stock codes
+
+
+def getAllStockCodes(url):
+    allCodes = []
     html = urllib.request.urlopen(url).read()
     html = html.decode('gbk')
     s = r'<li><a target="_blank" href="http://quote.eastmoney.com/\S\S(.*?).html">'
@@ -14,13 +16,15 @@ def urlTolist(url):
     code = pat.findall(html)
     for item in code:
         if item[0] == '6' or item[0] == '3' or item[0] == '0':
-            allCodeList.append(item)
-    return allCodeList
+            allCodes.append(item)
+    return allCodes
 
-# Retrive all stocks data
-def main():
+# Retrive all stock data
+
+
+def getAllStockData(date):
     stock_CodeUrl = 'http://quote.eastmoney.com/stocklist.html'
-    allCodelist = urlTolist(stock_CodeUrl)
+    allCodes = getAllStockCodes(stock_CodeUrl)
 
     currentFilePath = os.path.realpath(__file__)
     dirPath = os.path.dirname(currentFilePath)
@@ -28,22 +32,40 @@ def main():
     if not os.path.exists(dataDirPath):
         os.mkdir(dataDirPath)
 
+    # http://www.sse.com.cn/assortment/stock/list/share/
+    # http://www.szse.cn/main/marketdata/jypz/colist/
+    shStockIdsFilePath = os.path.join(dataDirPath, 'sh_stock_ids.xls')
     szStockIdsFilePath = os.path.join(dataDirPath, 'sz_stock_ids.xlsx')
+
     stocksDirPath = os.path.join(dataDirPath, 'stocks')
     if not os.path.exists(stocksDirPath):
         os.mkdir(stocksDirPath)
 
+    url = 'http://query.sse.com.cn/security/stock/downloadStockListFile.do?csrcCode=&stockCode=&areaName=&stockType=1'
+    # https://www.cnblogs.com/majianguo/p/8186429.html
+    opener = urllib.request.build_opener()
+    opener.addheaders = [
+        ("Referer", "http://www.sse.com.cn/assortment/stock/list/share/")
+    ]
+    originalOpener = urllib.request._opener
+    urllib.request.install_opener(opener)
+    urllib.request.urlretrieve(url, shStockIdsFilePath)
+    urllib.request.install_opener(originalOpener)
+
     url = 'http://www.szse.cn/szseWeb/ShowReport.szse?SHOWTYPE=xlsx&CATALOGID=1110&tab1PAGENO=1&ENCODE=1&TABKEY=tab1'
     urllib.request.urlretrieve(url, szStockIdsFilePath)
 
-    for code in allCodelist:
+    for code in allCodes:
         print('Retriving data of %s...' % code)
         if code[0] == '6':
             url = 'http://quotes.money.163.com/service/chddata.html?code=0'+code +\
-                '&end=20180413&fields=TCLOSE;HIGH;LOW;TOPEN;LCLOSE;CHG;PCHG;TURNOVER;VOTURNOVER;VATURNOVER;TCAP;MCAP'
+                '&end=' + date + '&fields=TCLOSE;HIGH;LOW;TOPEN;LCLOSE;CHG;PCHG;TURNOVER;VOTURNOVER;VATURNOVER;TCAP;MCAP'
         else:
             url = 'http://quotes.money.163.com/service/chddata.html?code=1'+code +\
-                '&end=20180413&fields=TCLOSE;HIGH;LOW;TOPEN;LCLOSE;CHG;PCHG;TURNOVER;VOTURNOVER;VATURNOVER;TCAP;MCAP'
-        urllib.request.urlretrieve(url, os.path.join(stocksDirPath, code +'.csv'))
+                '&end=' + date + '&fields=TCLOSE;HIGH;LOW;TOPEN;LCLOSE;CHG;PCHG;TURNOVER;VOTURNOVER;VATURNOVER;TCAP;MCAP'
+        urllib.request.urlretrieve(
+            url, os.path.join(stocksDirPath, code + '.csv'))
 
-main()
+
+if __name__ == '__main__':
+    getAllStockData('20180413')
